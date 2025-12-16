f90d05b9c686dd2bf6703d4db178c8da57d72e0c
Gyane-git <alpen850@gmail.com>
2025-12-16 12:51:25 +0545

    feat: add ImageSlider component and integrate it into Dashboard and Home pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 
+import ImageSlider from "@/components/ImageSlider";
+
+
 export default function Home() {
   return (
     <main>
+       <ImageSlider />
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Welcome to Yuemi Ecosystem Nepal</h1>
+       
+        <>Welcome to Yuemi Ecosystem Nepal</>
       </div>
     </main>
 
