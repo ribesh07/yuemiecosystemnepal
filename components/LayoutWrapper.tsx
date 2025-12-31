c1f384dc0cfacdef3e186450b9016022d3d4c8eb
Gyane-git <alpen850@gmail.com>
2025-12-31 11:34:54 +0545

    admin header bar update

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import HeaderBar from "@/components/HeaderBar";
+import Footer from "@/components/FooterBar";
+//import TawkToWidget from "@/components/TawkToWidget";
+import CartSidebar from "@/components/CartSidebar";
+import CookieConsentBanner from "@/components/CookieConsentBanner";
+import { Toaster } from "react-hot-toast";
+
+interface LayoutWrapperProps {
+  children: ReactNode;
+}
+
+export default function LayoutWrapper({ children }: LayoutWrapperProps) {
+  const pathname = usePathname();
+
+  // Check if route starts with `/admin`
+  const isAdminRoute = pathname?.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminRoute && <HeaderBar />}
+      {isAdminRoute && <CartSidebar />}
+      {children}
+      {isAdminRoute && <CookieConsentBanner />}
+      {!isAdminRoute && <Footer />}
+      {!isAdminRoute && <Toaster position="top-right" />}
+    </>
+  );
+}
