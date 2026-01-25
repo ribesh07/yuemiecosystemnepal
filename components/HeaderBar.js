2b2808c6e38d15543dce63fe0325ffdcd897b8fe
ribesh07 <sahribesh0123@gmail.com>
2026-01-25 13:31:29 +0545

    home

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -157,7 +157,7 @@ export default function Header() {
             {/* Desktop Navigation Links */}
             <div className="hidden md:flex items-center space-x-8 text-[14px]">
               <a
-                href="/"
+                href="/home"
                 className="text-orange-500 font-bold hover:underline cursor-pointer hover:text-orange-600 transition-colors"
               >
                 HOME
