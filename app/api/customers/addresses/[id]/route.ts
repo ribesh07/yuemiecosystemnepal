a618b0c11f02443530d40b231feb34f20df89009
Gyane-git <alpen850@gmail.com>
2026-02-18 16:35:40 +0545

    comment

--- app/api/customers/addresses/[id]/route.ts
+++ app/api/customers/addresses/[id]/route.ts
@@ -1,25 +1,25 @@
-import { prisma } from "@/prisma/prisma-client"
-import { NextResponse } from "next/server";
+// import { prisma } from "@/prisma/prisma-client"
+// import { NextResponse } from "next/server";
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const body = await req.json();
+// export async function PUT(
+//   req: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const body = await req.json();
 
-  const updated = await prisma.customerAddress.update({
-    where: { id: BigInt(params.id) },
-    data: {
-      fullName: body.fullName,
-      phone: body.phone,
-      provinceId: BigInt(body.provinceId),
-      cityId: BigInt(body.cityId),
-      zoneId: BigInt(body.zoneId),
-      address: body.address,
-      landmark: body.landmark,
-      updatedAt: new Date(),
-    },
-  });
+//   const updated = await prisma.customerAddress.update({
+//     where: { id: BigInt(params.id) },
+//     data: {
+//       fullName: body.fullName,
+//       phone: body.phone,
+//       provinceId: BigInt(body.provinceId),
+//       cityId: BigInt(body.cityId),
+//       zoneId: BigInt(body.zoneId),
+//       address: body.address,
+//       landmark: body.landmark,
+//       updatedAt: new Date(),
+//     },
+//   });
 
-  return NextResponse.json(updated);
-}
+//   return NextResponse.json(updated);
+// }
